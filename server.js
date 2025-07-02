849d62524d69eba91527ed5418dad1c2fa4c5b35
Geovani Peña Ramirez <82411729+THENeogeo@users.noreply.github.com>
2025-07-02 15:15:03 -0600

    Sistema

--- server.js
+++ server.js
@@ -101,5 +101,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅✅Servidor en http://localhost:${PORT}✅✅`);
+  console.log(`✅✅Servidor corriendo en http://localhost:${PORT}✅✅`);
 });
